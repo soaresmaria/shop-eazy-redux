27d7a572bfa4f72ede4f674e51c71ae444de0934
Mona Lisa <soaresmaria181@yahoo.com>
2021-11-18 03:14:06 -0500

    implementing product and category storage

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-
+import { useDispatch, useSelector } from 'react-redux';
 import ProductItem from '../ProductItem';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
+import { idbPromise } from "../../utils/helpers";
 
 function ProductList({ currentCategory }) {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   const products = data?.products || [];
+
+  useEffect(() => {
+    if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products
+      });
+
+      // but let's also take each product and save it to IndexedDB using the helper function 
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+    } else if (!loading) {
+      idbPromise('products', 'get').then((products) => {
+        // use retrieved data to set global state for offline browsing
+        console.log("I am offline")
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products
+        });
+      })
+    }
+  }, [loading, data, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useQuery } from '@apollo/react-hooks';
 import Cart from "../components/Cart";
 import { useDispatch, useSelector } from 'react-redux';
 import {
@@ -10,22 +10,50 @@ import {
   UPDATE_PRODUCTS,
 } from "../utils/actions";
 import { QUERY_PRODUCTS } from '../utils/queries';
+import { idbPromise } from "../utils/helpers";
 import spinner from '../assets/spinner.gif';
 
 function Detail() {
+
+  const state = useSelector((state) => {
+    return state
+  });
+  const dispatch = useDispatch();
+
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const products = data?.products || [];
+  const { products, cart } = state;
 
   useEffect(() => {
+    // already in global store
     if (products.length) {
-      setCurrentProduct(products.find((product) => product._id === id));
+      setCurrentProduct(products.find(product => product._id === id));
+    } 
+    // retrieved from server
+    else if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products
+      });
+
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
     }
-  }, [products, id]);
+    // get cache from idb
+    else if (!loading) {
+      idbPromise('products', 'get').then((indexedProducts) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: indexedProducts
+        });
+      });
+    }
+  }, [products, data, loading, dispatch, id]);
 
   const addToCart = () => {
     const itemInCart = cart.find((cartItem) => cartItem._id === id)
@@ -33,6 +61,10 @@ function Detail() {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+      idbPromise('cart', 'put', {
+        ...itemInCart,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
     } else {
@@ -44,7 +76,6 @@ function Detail() {
 
     }
   }
-
 
   const removeFromCart = () => {
     dispatch({
